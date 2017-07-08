1e5e9a5554a033866eb2e5a5df4046fb42d5f3ff
Bill Hefty <billhefty@gmail.com>
2017-07-08 12:05:16 -0500

    fetch translation from api and return to user

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+'use strict'
+
 const express = require('express')
+require('dotenv').config()
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 const app = express()
+
+const API_KEY = process.env.X_MASHAPE_KEY
+
+const getTranslation = (text) => {
+  const url = `https://yoda.p.mashape.com/yoda?sentence=${encodeURIComponent(text)}`
+  const options = {
+    headers: {
+      'X-Mashape-Key': API_KEY,
+      'Accept': 'text/plain'
+    }
+  }
+
+  return fetch(url, options)
+    .then(response => response.text())
+}
 
 app.get('/api/:text', (req, res) => {
   let text = req.params.text
-
-  let result = {
-    translation: text.toUpperCase()
-  }
-
-  res.send(result)
+  getTranslation(text)
+    .then(response => response.trim())
+    .then(translation => {
+      let result = { translation }
+      console.log(result)
+      res.send(result)
+    })
 })
 
 app.listen(process.env.PORT || 8080, () => {
